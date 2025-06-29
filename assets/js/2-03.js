03c449a36f6483a187d02a592e0836b3add0a98d
Vlad <vlad086367@gmail.com>
2025-06-29 22:51:50 +0300

    Task 1

--- assets/js/2-03.js
+++ assets/js/2-03.js
@@ -1,2 +1,20 @@
 
 // 1. Вивести числа від 25 до 0 (порядок зменшення).
+
+const min_element = 0;
+let number = 25;
+
+// while (number >= min_element) {
+//     console.log('number :>> ', number);
+//     number--;
+// }
+
+
+// do {
+//     console.log('number :>> ', number);
+//     number--
+// } while (number >= min_element)
+
+for (let number = 25; number >= min_element; number--) {
+    console.log('number :>> ', number);
+}
