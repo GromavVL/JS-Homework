4c43aa188f67e8a79472795b1cff3d7dc7314860
Vlad <vlad086367@gmail.com>
2025-06-30 13:35:47 +0300

    Task 2

--- assets/js/2-03.js
+++ assets/js/2-03.js
@@ -4,16 +4,15 @@
 // const min_element = 0;
 // let number = 25;
 
-// // while (number >= min_element) {
-// //     console.log('number :>> ', number);
-// //     number--;
-// // }
+// while (number >= min_element) {
+//     console.log('number :>> ', number);
+//     number--;
+// }
 
-
-// // do {
-// //     console.log('number :>> ', number);
-// //     number--
-// // } while (number >= min_element)
+// do {
+//     console.log('number :>> ', number);
+//     number--
+// } while (number >= min_element)
 
 // for (let number = 25; number >= min_element; number--) {
 //     console.log('number :>> ', number);
@@ -22,7 +21,22 @@
 
 // 2. Вивести числа від 10 до 50, які кратні 5.
 
+// let = i = 10;
 
+// while (i <= 50) {
+//     console.log('i :>> ', i);
+//     i = i + 5;
+// }
+
+// do {
+//     console.log('i :>> ', i);
+//     i = i + 5;
+// } while (i <= 50)
+
+
+// for (let i = 10; i <= 50; i = i + 5) {
+//     console.log('i :>> ', i);
+// }
 
 // 3. *Знайти суму чисел в межах від 1 до 100.
 
@@ -35,13 +49,11 @@
 // }
 // console.log('suma :>> ', suma);
 
-
 // do {
 //     suma = suma + i;
 //     i++;
 // } while (i <= 100);
 // console.log('suma :>> ', suma);
-
 
 // for (let i = 1; i <= 100; i++) {
 //     suma = suma + i;
