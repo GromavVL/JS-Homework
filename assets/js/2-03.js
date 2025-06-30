36ed9b4a3c587d1587c14a3075b889e2bda07ab6
Vlad <vlad086367@gmail.com>
2025-06-30 13:00:58 +0300

    Task 3

--- assets/js/2-03.js
+++ assets/js/2-03.js
@@ -1,20 +1,49 @@
 
-// 1. Вивести числа від 25 до 0 (порядок зменшення).
+// // 1. Вивести числа від 25 до 0 (порядок зменшення).
 
-const min_element = 0;
-let number = 25;
+// const min_element = 0;
+// let number = 25;
 
-// while (number >= min_element) {
+// // while (number >= min_element) {
+// //     console.log('number :>> ', number);
+// //     number--;
+// // }
+
+
+// // do {
+// //     console.log('number :>> ', number);
+// //     number--
+// // } while (number >= min_element)
+
+// for (let number = 25; number >= min_element; number--) {
 //     console.log('number :>> ', number);
-//     number--;
 // }
 
 
-// do {
-//     console.log('number :>> ', number);
-//     number--
-// } while (number >= min_element)
+// 2. Вивести числа від 10 до 50, які кратні 5.
 
-for (let number = 25; number >= min_element; number--) {
-    console.log('number :>> ', number);
-}
+
+
+// 3. *Знайти суму чисел в межах від 1 до 100.
+
+// let i = 1;
+// let suma = 0;
+
+// while ( i <= 100) {
+//     suma = suma + i;
+//     i++;
+// }
+// console.log('suma :>> ', suma);
+
+
+// do {
+//     suma = suma + i;
+//     i++;
+// } while (i <= 100);
+// console.log('suma :>> ', suma);
+
+
+// for (let i = 1; i <= 100; i++) {
+//     suma = suma + i;
+// }
+// console.log('suma :>> ', suma);
