5aebdfc25be06c1122e4b7b303d71c576ea673af
Vlad <vlad086367@gmail.com>
2025-07-02 16:01:57 +0300

    2-04 Task 2

--- assets/js/2-04.js
+++ assets/js/2-04.js
@@ -12,3 +12,17 @@
 // const people = isWorkingAgePerson(19);
 // console.log('people :>> ', people);
 
+
+// 2) Реалізувати функцію checkMultiplicity, яка приймає два числа і перевіряє, чи ділиться перше на друге націло (без залишку)
+
+// function checkMultiplicity(a, b) {
+//     if(b === 0) {
+//         return false;
+//     }
+//     return a % b === 0;
+// }
+
+// console.log(checkMultiplicity(25, 5)); // true
+// console.log(checkMultiplicity(15, 5)); // true
+// console.log(checkMultiplicity(15, 3)); // true 
+// console.log(checkMultiplicity(15, 4)); // false
