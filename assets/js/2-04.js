e1c7acc0c6850b673ceb40fc407553d1c7fd21e2
Vlad <vlad086367@gmail.com>
2025-07-02 14:50:46 +0300

    2-04 Task 1

---
+++ assets/js/2-04.js
@@ -0,0 +1,14 @@
+
+// 1) Реалізувати функцію isWorkingAgePerson, яка буде перевіряти, чи працездатного людина віку (наприклад, від 16 до 64)
+
+// function isWorkingAgePerson(age) {
+//     if (age >= 16 && age <= 64){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// const people = isWorkingAgePerson(19);
+// console.log('people :>> ', people);
+
