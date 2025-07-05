863b054a75e1440f4f01c594c28a997c5c9157ea
Vlad <vlad086367@gmail.com>
2025-07-05 17:19:24 +0300

    Object cart Task1

--- assets/js/2-05.js
+++ assets/js/2-05.js
@@ -33,12 +33,22 @@ customer.isMale = true;
 console.log('customer :>> ', customer);
 
 // Видалити у об'єкта властивість адреса
-delete customer.address;
+// delete customer.address;
 
 
 // Зробити копії об'єкту customer двома різними способами
-const customer2 = {...customer};
+const customer2 = { ...customer };
 
 const customer3 = Object.assign({}, customer);
 
+
 // * Реалізувати картку з відомостями про customer
+const userscart = document.querySelector('.cart')
+const content = `<p><span>Name: </span> ${customer.name}</p>
+            <p><span>Surname: </span> ${customer.surname}</p>
+            <p><span>Email: </span> ${customer.email}</p>
+            <p><span>Password: </span>${customer.password}</p>
+            <p><span>Phone: </span>${customer.phone}</p>
+            <p><span>Address: </span>${customer.address.city}, 
+            ${customer.address.street}, ${customer.address.house}</p>`;
+userscart.insertAdjacentHTML('beforeend', content);
