f45c319e8c7d44291e5f82e0e1f40b6f2a92453d
Vlad <vlad086367@gmail.com>
2025-07-05 16:03:17 +0300

    Objects

--- assets/js/2-05.js
+++ assets/js/2-05.js
@@ -1,1 +1,44 @@
 //------------JS Об'єкти-----------
+
+const customer = {
+    name: "Vladislav",
+    surname: "Ilin",
+    email: "vlad086367@gmail.com",
+    password: "vl83he7ej",
+    phone: "0989093542",
+    address: {
+        city: "Zaporizhzhia",
+        street: "Hor'kivske shose",
+        house: 32,
+        flat: 58,
+    },
+};
+
+// методи:
+
+// виведення адреси
+console.log(`Address: ${customer.address.city}, 
+    ${customer.address.street},
+    ${customer.address.house},
+    ${customer.address.flat}
+    `);
+// console.log(customer.address);
+
+// зміна номера телефону
+customer.phone = '4378353637';
+console.log('customer :>> ', customer);
+
+// Додати об'єкту властивість isMale
+customer.isMale = true;
+console.log('customer :>> ', customer);
+
+// Видалити у об'єкта властивість адреса
+delete customer.address;
+
+
+// Зробити копії об'єкту customer двома різними способами
+const customer2 = {...customer};
+
+const customer3 = Object.assign({}, customer);
+
+// * Реалізувати картку з відомостями про customer
