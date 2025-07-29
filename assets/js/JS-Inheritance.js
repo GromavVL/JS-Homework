050d3b04c8fcc9c62706c673f4a5419348e835f1
Vlad <vlad086367@gmail.com>
2025-07-29 15:47:25 +0300

    Completed all functionality

--- assets/js/JS-Inheritance.js
+++ assets/js/JS-Inheritance.js
@@ -8,24 +8,48 @@ class Vehicle {
         this._manufactureDate = manufactureDate;
     }
     getFullInfo() {
-        return `[Габарити - ${this._dimensions}], [Марка - ${this._brand}], [Модель - ${this._model}], [Дата виробництва - ${this._manufactureDate}]`;
+        return ` [Марка - ${this._brand}], [Модель - ${this._model}], [Габарити - ${this._dimensions}], [Вік виробництва - ${this._manufactureDate}]`;
     }
-    //  getAge() - повертає кількість років із дня виробництва, return за допомою ШІ)
+    //  getAge() - повертає кількість років із дня виробництва
     get getAge() {
-        const currentYear = new Date().getFullYear();
-        return currentYear - this._manufactureDate;
+        return new Date().getFullYear() - this._manufactureDate;
     }
 }
 const car = new Vehicle('2T', 'Audi', 'A8', 2020);
-console.log('car :>> ', car.getAge);
+console.log('car :>> ', car.getFullInfo());
 
-class PassengerTransport extends Vehicle{
-    constructor(passengerLimit, passengerCount, dimensions, brand, model, manufactureDate) {
-        super(dimensions, brand, model, manufactureDate);
+class PassengerTransport extends Vehicle {
+    constructor(passengerLimit, passengerCount, brand, model, manufactureDate) {
+        super(brand, model, manufactureDate);
         this._passengerLimit = passengerLimit;
         this._passengerCount = passengerCount;
     }
     addPassenger() {
-        
+        if (this._passengerCount < this._passengerLimit) {
+            this._passengerCount++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    getFullInfo() {
+        return `[Марка - ${this._brand}], [Модель - ${this._model}], [Кількість місць - ${this._passengerLimit}], [Вік виробництва - ${this._manufactureDate}]`;
     }
 }
+const car2 = new PassengerTransport(5, 3, '2T', 'Mercedes', 'GT63', 2019);
+console.log('car2 :>> ', car2.getFullInfo());
+
+class FreightTransport extends Vehicle {
+    constructor(capacity, brand, model, manufactureDate) {
+        super(brand, model, manufactureDate);
+        this._capacity = capacity;
+    }
+    getFullInfo() {
+        return ` [Марка - ${this._brand}], [Модель - ${this._model}], [Вік виробництва - ${this._manufactureDate}], [Вантажопідйомність - ${this._capacity}]`;
+    }
+    checkLoadingPossibility(weight) {
+        return weight <= this._capacity;
+    }
+}
+const car3 = new FreightTransport(2000, 'Dodge', 'RAM 1500', 2021);
+console.log('car3 :>> ', car3.getFullInfo());
