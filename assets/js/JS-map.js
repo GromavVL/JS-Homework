bac24221590181ea0d5299cffcf111c815735733
Vlad <vlad086367@gmail.com>
2025-08-03 23:17:13 +0300

    Task 2 completed

--- assets/js/JS-map.js
+++ assets/js/JS-map.js
@@ -4,7 +4,7 @@ const map1 = new Map();
 map1.set(1, 'first').set(2, 'second').set(3, 'third');
 
 // Отримайте кількість елементів
-console.log('Кількість елементів :>> ',  map1.size);
+console.log('Кількість елементів :>> ', map1.size);
 
 // Додати та видалити елемент
 map1.set(4, 'fourth');
@@ -23,6 +23,22 @@ console.log('mapKeys :>> ', mapKeys);
 console.log('mapValue :>> ', mapValue);
 
 
-const mapText = function (...arg) {
-    
+
+const m1 = new Map();
+m1.set('1', 'first').set('3', 'third');
+const text = 'This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.';
+
+
+const mapText = function (text) {
+    const result = text.split(' ');
+    console.log('result :>> ', result);
+    const arr1 = result.map(function(item){
+        if(m1.has(item)) {
+            return m1.get(item)
+        } else {
+            return item;
+        }
+    });
+    return arr1.join(' ');
 }
+console.log(mapText(text));
