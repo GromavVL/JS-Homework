21cf50250e857914711243bef0d5c61030d32f9f
Vlad <vlad086367@gmail.com>
2025-08-20 21:48:18 +0300

    Completed Users array

--- assets/js/JS-UsersArray.js
+++ assets/js/JS-UsersArray.js
@@ -26,31 +26,39 @@ for (let i = 0; i < 10; i++) {
 console.table(users);
 
 // Вивести список повних імен користувачів.
-User.prototype.getFullName = function (){
-    return `${this.firstName} ${this.lastName}`;
+Array.prototype.getFullName = function () {
+    return this.map(user => `${user.firstName}, ${user.lastName}`);
 }
+console.table(users.getFullName());
 
 // Отримати масив користувачів, які не підписані (not subscribed)
-User.prototype.getIsSubscribed = function() {
-    return this.isSubscribed;
-}
+const notsubscribed = users.filter(user => !user.isSubscribed);
+console.table(notsubscribed);
 
 // Отримати масив повних імен осіб жіночої статі шкільного віку (6 – 18 років).
-User.prototype.getAgeName = function() {
-    if(this.isMale !== true) {
-        if (this.age >= 6 && this.age <= 18){
-            return `${this.firstName} ${this.lastName}`;
-        }
-    }
+const isMaleSchool = users
+    .filter(user => !user.isMale && user.age >= 6 && user.age <= 18)
+    .map(user => `${user.firstName}, ${user.lastName}`);
+console.table(isMaleSchool);
+
+
+// Змінити email користувачу з id 2
+// const foundIndex = users.findIndex(user => user.id === 2);
+// users[foundIndex] = 'newemail@gmail.com';
+
+// Видалити з масиву користувача з email  useremail5@gmail.com
+const filteredUsers = users.filter(user => user.email !== 'useremail0@gmail.com');
+console.table(filteredUsers);
+
+// * Знайти середній вік користувачів
+const sumAge = users.reduce((sum, user) => sum + user.age, 0);
+const avgSum = sumAge / users.length;
+console.table(avgSum);
+
+// * Впорядкувати користувачів за віком
+function compareNumeric(a, b) {
+    if (a.age > b.age) return 1;
+    if(a.age === b.age) return 0;
+    if (a.age < b.age) return -1;
 }
-
-// const fullNames = users.map(user => user.getFullName());
-// console.table(fullNames);
-
-// const notSubscribed = users.filter(user => !user.getIsSubscribed()); 
-// console.table(notSubscribed);
-
-
-// Впорядкувати користувачів за віком (sort).
-const sortedAge = [...users].sort(user => user.age);
-console.table(sortedAge);
+console.table(users.sort(compareNumeric));
